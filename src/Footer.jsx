0ec35e3e2cb39a7e6002468fde9f5a9a9fc83f7d
Het Soni <94887121+hetsoni1101@users.noreply.github.com>
2022-07-25 13:29:06 +0530

    Add files via upload

---
+++ src/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Footer() {
+    return (
+        <>
+            <div className="footer_p">
+                <p>Created by Het Soni||All Right Reserved ©️ 2022</p>
+            </div>
+        </>
+    )
+}
+
+export default Footer
